02717e9004fb8988c9066ea4facbf2a23ec17728
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2022-12-27 23:26:31 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -3,13 +3,14 @@ import ListItems from './ListItems';
 //code here
  const List = ({listx}) =>{
   return(
-   {
- 
-    listx.map((item , idx)=>{
-     return  <ListItems valuex={item} index={idx} />
-    })
     
-   }
+    <>
+      <ul id='list'>
+        {listx.map((list  => <ListItems valuex ={list}/>))}
+      </ul>
+    </>
+    
+  
   )
 }
 
